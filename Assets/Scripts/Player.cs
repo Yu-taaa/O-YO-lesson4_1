0853eb488c64a7260fe01594a7677417c4723d8f
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-02-07 13:37:56 +0900

    end

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,1 +1,1 @@
-﻿using UnityEngine;  using System.Collections;    public class Player : MonoBehaviour {     	void OnCollisionEnter(Collision other){ 		if (other.gameObject.tag == "Enemy") {  			Destroy (other.gameObject); 		}   	}   }
+﻿using UnityEngine; using System.Collections;  public class Player : MonoBehaviour {  	public bool getDestroy = false;  	void OnCollisionEnter(Collision  other){ 		if (other.gameObject.tag == "Enemy") {  			Destroy (other.gameObject); 			getDestroy = true; 		} 	} } 
